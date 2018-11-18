ea54f9414c0cfa7854d97cbf788b3a6aa2e3e893
YudiPavel <pavelyudin298@gmail.com>
2018-11-18 17:01:55 +0300

    static news

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-blog';
+  login:boolean = false;
+
+  name:string = "";
+  password:string = "";
+
+  enterFunc(){
+    if(this.name === 'Pavel' && this.password === '1234'){
+      this.login = true;
+      console.log("YES!");
+    }
+    else{
+      console.log("No... but YES!");
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsComponent } from './news/news.component';
+import { MyPageComponent } from './my-page/my-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+
+const appRoutes: Routes = [
+  {path: '', component: NewsComponent},
+  {path: 'my-page', component: MyPageComponent},
+  {path: 'registration', component: RegistrationComponent},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    MyPageComponent,
+    NotFoundComponent,
+    RegistrationComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
